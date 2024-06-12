1abd3c0e6322ccfe156b045b875425d9fa851313
wotjr1649 <wotjr980321@naver.com>
2024-06-13 04:44:12 +0900

    update 湲곕뒫 援ы쁽

---
+++ app/routes/api/update_api.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from auth.jwt import verify_access_token
+from database import mysql_conn
+from fastapi import BackgroundTasks, Depends, Header
+from routes.apihelper import base64_to_uuid
+from routes.apihelper.update_apihelpler import update_mysql_and_messaging
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def update_mbti(
+    background_tasks: BackgroundTasks,
+    jwt: Annotated[str, Header(convert_underscores=False)] = None,
+    mbti: str = None,
+    mysql_db: AsyncSession = Depends(mysql_conn.get_db),
+):
+    user_id = base64_to_uuid(verify_access_token(jwt).get("token"))
+    return await update_mysql_and_messaging(user_id, mbti, background_tasks, mysql_db)
---
+++ app/routes/apihelper/update_apihelpler.py
@@ -0,0 +1,60 @@
+from database import mongo_conn
+from fastapi import BackgroundTasks, HTTPException
+from models.table import UserModel
+from routes.apihelper import message, produce_messages
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+mbti_type = {
+    "ISTP": "ISTP",
+    "ISFP": "ISFP",
+    "INFP": "INFP",
+    "INTP": "INTP",
+    "ESTP": "ESTP",
+    "ESFP": "ESFP",
+    "ENFP": "ENFP",
+    "ENTP": "ENTP",
+    "ISTJ": "ISTJ",
+    "ISFJ": "ISFJ",
+    "INFJ": "INFJ",
+    "INTJ": "INTJ",
+    "ESTJ": "ESTJ",
+    "ESFJ": "ESFJ",
+    "ENFJ": "ENFJ",
+    "ENTJ": "ENTJ",
+}
+
+
+async def update_mysql_and_messaging(
+    user_id: str, mbti: str, background_tasks: BackgroundTasks, mysql_db: AsyncSession
+):
+    if not mbti_type.get(mbti.upper(), None):
+        raise HTTPException(status_code=400, detail=f"mbti 유형에 맞는 문자열이 아닙니다.")
+    # MongoDB에서 유저 확인
+    result = await mongo_conn.member["user"].find_one({"_id": user_id, "is_delete": False})
+    if result:
+        async with mysql_db as session:
+            try:
+                result = (
+                    await session.execute(select(UserModel).filter_by(user_id=user_id))
+                ).scalar_one_or_none()
+                if result:
+                    result.mbti = mbti_type.get(mbti.upper())
+                    await session.commit()
+                    print(f"Updated user_id {user_id} with new MBTI: {mbti_type.get(mbti.upper())}")
+                update_time = (
+                    await session.execute(select(UserModel).filter_by(user_id=user_id))
+                ).scalar_one_or_none()
+                model = {
+                    "user_id": result.user_id,
+                    "mbti": mbti,
+                    "last_update": update_time.last_update.strftime("%Y-%m-%dT%H:%M:%S"),
+                }
+                background_tasks.add_task(produce_messages, [message("update", "user", model)])
+                return "mbti가 업데이트 되었습니다."
+            except:
+                raise HTTPException(status_code=400, detail=f"Mysql에 데이터가 존재하지 않습니다")
+            finally:
+                await session.close()
+    else:
+        raise HTTPException(status_code=400, detail=f"MongoDB에 데이터가 존재하지 않습니다")
